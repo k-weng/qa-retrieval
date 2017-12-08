73f85c1443f616a161b5e9f82b1b8587caf86fb1
Kevin Weng <kweng@mit.edu>
2017-12-08 14:03:08 -0500

    added sudo in front of command

--- get_datasets.sh
+++ get_datasets.sh
@@ -3,7 +3,7 @@
 unbuntu="https://github.com/taolei87/askubuntu.git"
 android="https://github.com/jiangfeng1124/Android.git"
 
-git clone "$unbuntu"
-git clone "$android"
+sudo git clone "$unbuntu"
+sudo git clone "$android"
 
 mv Android/ android/
