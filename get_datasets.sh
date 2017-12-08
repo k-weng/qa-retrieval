fa0d542cbc7b3734841f56ffe84a11e90ff95b64
Kevin Weng <kweng@mit.edu>
2017-12-08 13:50:03 -0500

    added bash script to download datasets

---
+++ get_datasets.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+unbuntu="https://github.com/taolei87/askubuntu.git"
+android="https://github.com/jiangfeng1124/Android.git"
+
+git clone "$unbuntu"
+git clone "$android"
+
+mv Android/ android/
